9ae9f69895bf42151cc1a5876553c2a8912b0f64
jwbat <jwesleyb@gmail.com>
2021-12-05 09:17:27 -0800

    .

--- cdata/main.cpp
+++ cdata/main.cpp
@@ -7,17 +7,22 @@ using std::setw;
 
 int main() {
 
-    cout << "\n"; 
-    cout << "\n" << setw(25) << "sizeof bool:"         << setw(10) << sizeof(bool);
-    cout << "\n" << setw(25) << "sizeof char:"         << setw(10) << sizeof(char);
-    cout << "\n" << setw(25) << "sizeof short:"        << setw(10) << sizeof(short);
-    cout << "\n" << setw(25) << "sizeof wchar_t:"      << setw(10) << sizeof(wchar_t);
-    cout << "\n" << setw(25) << "sizeof int:"          << setw(10) << sizeof(int);
-    cout << "\n" << setw(25) << "sizeof float:"        << setw(10) << sizeof(float);
-    cout << "\n" << setw(25) << "sizeof double:"       << setw(10) << sizeof(double);
-    cout << "\n" << setw(25) << "sizeof long long:"    << setw(10) << sizeof(long long);
-    cout << "\n" << setw(25) << "sizeof long double:"  << setw(10) << sizeof(long double);
-    cout << "\n\n";
+    cout << "\n" << setw(25) << "type"                 << setw(10) << "size"
+         << "\n" << setw(25) << "----"                 << setw(10) << "----"
+         << "\n" << setw(25) << "bool:"                << setw(10) << sizeof(bool)
+         << "\n" << setw(25) << "char:"                << setw(10) << sizeof(char)
+         << "\n" << setw(25) << "unsigned char:"       << setw(10) << sizeof(unsigned char)
+         << "\n" << setw(25) << "signed char:"         << setw(10) << sizeof(signed char)
+         << "\n" << setw(25) << "char8_t:"             << setw(10) << sizeof(char8_t)
+         << "\n" << setw(25) << "short:"               << setw(10) << sizeof(short)
+         << "\n" << setw(25) << "wchar_t:"             << setw(10) << sizeof(wchar_t)
+         << "\n" << setw(25) << "float:"               << setw(10) << sizeof(float)
+         << "\n" << setw(25) << "int:"                 << setw(10) << sizeof(int)
+         << "\n" << setw(25) << "long:"                << setw(10) << sizeof(long)
+         << "\n" << setw(25) << "double:"              << setw(10) << sizeof(double)
+         << "\n" << setw(25) << "long long:"           << setw(10) << sizeof(long long)
+         << "\n" << setw(25) << "long double:"         << setw(10) << sizeof(long double)
+         << "\n\n";
 
     return 0;
 }
