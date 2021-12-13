e94dde80dcf3364502e0b5f3c2067b9b18b09341
jwbat <jwesleyb@gmail.com>
2021-12-12 16:30:22 -0800

    .

--- cdata/main.cpp
+++ cdata/main.cpp
@@ -8,9 +8,10 @@ using std::numeric_limits;
 template <typename T>
 void print_type_properties()
 {
-    cout << "\n\t size: " << setw(24)  << sizeof(T)
-         << "\n\t  min: " << setw(24) << numeric_limits<T>::min()
-         << "\n\t  max: " << setw(24) << numeric_limits<T>::max() << "\n";
+    cout << "\n\t  size: " << setw(24)  << sizeof(T)
+         << "\n\tdigits: " << setw(24) << numeric_limits<T>::digits10
+         << "\n\t   min: " << setw(24) << numeric_limits<T>::min()
+         << "\n\t   max: " << setw(24) << numeric_limits<T>::max() << "\n";
 }
 
 auto newline = [] (int n = 1) { for (int i{ 0 }; i < n; ++i) cout << "\n"; };
@@ -20,43 +21,43 @@ int main()
     cout << "\033[1;31m";
     newline();
     
-    cout << setw(40) << " int ";
-    cout << "\n\t ------------------------------";
+    cout << setw(40) << "   int";
+    cout << "\n\t -------------------------------";
     print_type_properties<int>();
 
     newline(2);
-    cout << setw(40) << " long long ";
-    cout << "\n\t ------------------------------";
+    cout << setw(40) << "   long long";
+    cout << "\n\t -------------------------------";
     print_type_properties<long long>();
 
     newline(2);
-    cout << setw(40) << " unsigned long / size_t ";
-    cout << "\n\t ------------------------------";
+    cout << setw(40) << "   unsigned long / size_t";
+    cout << "\n\t -------------------------------";
     print_type_properties<size_t>();
 
     newline(2);
-    cout << setw(40) << " double ";
-    cout << "\n\t ------------------------------";
+    cout << setw(40) << "   double";
+    cout << "\n\t -------------------------------";
     print_type_properties<double>();
 
     newline(2);
-    cout << setw(40) << " long double ";
-    cout << "\n\t ------------------------------";
+    cout << setw(40) << "   long double";
+    cout << "\n\t -------------------------------";
     print_type_properties<long double>();
 
     newline(2);
-    cout << setw(40) << " float ";
-    cout << "\n\t ------------------------------";
+    cout << setw(40) << "   float";
+    cout << "\n\t -------------------------------";
     print_type_properties<float>();
 
     newline(2);
-    cout << setw(40) << " unsigned short ";
-    cout << "\n\t ------------------------------";
+    cout << setw(40) << "   unsigned short";
+    cout << "\n\t -------------------------------";
     print_type_properties<unsigned short>();
 
     newline(2);
-    cout << setw(40) << " bool ";
-    cout << "\n\t ------------------------------";
+    cout << setw(40) << "  bool";
+    cout << "\n\t -------------------------------";
     print_type_properties<bool>();
 
 
