70b2683566fbb31a75e7cec542f262bfdacfe959
jwbat <jwesleyb@gmail.com>
2021-12-18 08:44:15 -0800

    .

--- fold.h
+++ fold.h
@@ -5,6 +5,10 @@ using std::forward; using std::move; using std::accumulate;
 template <typename F, typename R, typename T>
 constexpr T foldl(F&& f, R&& range, T init)
 {
-    return accumulate(begin(range), end(range), move(init), forward<F>(f));
+    return accumulate(
+              begin(range),
+              end(range),
+              move(init),
+              forward<F>(f)
+            );
 }
-
