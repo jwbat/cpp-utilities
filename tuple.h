b0ed2a29d0dd76c587c4979dc91e1529d202a17b
James <jwesleyb@gmail.com>
2021-12-27 15:35:03 -0800

    .

--- tuple.h
+++ tuple.h
@@ -5,22 +5,24 @@
 using std::tuple; using std::ostream; using std::get;
 using std::tuple_size;
 
-
 template<size_t> struct int_{};
 
 template <class Tuple, size_t Pos>
-ostream& print_tuple(ostream& out, const Tuple& t, int_<Pos> ) {
+ostream& print_tuple(ostream& out, const Tuple& t, int_<Pos> ) 
+{
   out << get< tuple_size<Tuple>::value - Pos >(t) << ", ";
   return print_tuple(out, t, int_<Pos - 1>());
 }
 
 template <class Tuple>
-ostream& print_tuple(ostream& out, const Tuple& t, int_<1> ) {
+ostream& print_tuple(ostream& out, const Tuple& t, int_<1> ) 
+{
   return out << get<tuple_size<Tuple>::value - 1>(t);
 }
 
 template <class... Args>
-ostream& operator<< (ostream& out, const tuple<Args...>& t) {
+ostream& operator<< (ostream& out, const tuple<Args...>& t) 
+{
   out << '('; 
   print_tuple(out, t, int_<sizeof...(Args)>()); 
   return out << ')';
