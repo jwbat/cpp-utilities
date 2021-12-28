c4276b6a934692f71279c2472ff29bece52e31e4
James <jwesleyb@gmail.com>
2021-12-28 14:25:29 -0800

    .

---
+++ show_compiler.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using std::cout;
+
+
+void show_compiler()
+{
+    #if defined _MSC_VER
+        cout << "Visual C++ \n";
+    #elif defined __clang__
+        cout << "Clang \n";
+    #elif defined __GNUG__
+        cout << "GCC \n";
+    #else
+        cout << "Unknown compiler \n";
+    #endif
+}
