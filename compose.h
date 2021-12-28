b2484ee3216d502ceb8f29ea7b779e882b627852
James <jwesleyb@gmail.com>
2021-12-28 13:28:26 -0800

    .

--- compose.h
+++ compose.h
@@ -1,3 +1,6 @@
+#include <utility>
+using std::forward;
+
 template <typename F, typename G>
 auto compose(F&& f, G&& g)
 {
@@ -10,7 +13,34 @@ auto compose(F&& f, R&&... r)
     return [=] (auto x) { return f(compose(r...)(x)); };
 }
 
+template <typename F, typename G>
+auto compose(F&& f, G&& g)
+{
+    return [=] (auto x) { return f(g(x)); };
+}
+
+template <typename F, typename... R>
+auto compose(F&& f, R&&... r)
+{
+    return [=] (auto x) { return f(compose(r...)(x)); };
+}
+
+template <typename F, typename G>
+auto operator*(F&& f, G&& g)
+{
+    return compose(forward<F>(f), forward<G>(g));
+}
+
+template <typename F, typename... R>
+auto operator*(F&& f, R&&... r)
+{
+    return operator*(forward<F>(f), r...);
+}
 
 /*
- *  compose(f, g, h)(x)
+ *  auto ans = (f * g * h)(x);
+*/
+//              or
+/*
+ *  auto ans = compose(f, g, h)(x);
 */
