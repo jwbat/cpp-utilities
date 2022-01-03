d1a6e8bf93447708c4b930d6a44843ac1f097082
jwbat <jwesleyb@gmail.com>
2022-01-03 15:37:32 -0800

    .

--- toggler.h
+++ toggler.h
@@ -1,9 +1,6 @@
-auto toggler(int a, int b) {
+auto toggler(int a, int b) 
+{
     const int t = a ^ b;
     int x = b; 
     return [x, t] () mutable { x ^= t; return x; };
-}
-
-auto fill(int i) {
-    return [i] (auto n) { return n = i; }; 
 }
