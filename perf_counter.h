23966213c6a2e84ece5eeaf87cb89c941376b946
jwbat <jwesleyb@gmail.com>
2022-01-02 19:25:24 -0800

    .

--- perf_counter.h
+++ perf_counter.h
@@ -1,22 +1,24 @@
 #pragma once
 #include <chrono>
 
+using namespace std::chrono;
 
-template <typename Time = std::chrono::microseconds,
-          typename Clock = std::chrono::high_resolution_clock>
-struct perf_timer {
+auto start = high_resolution_clock::now();
+auto stop = high_resolution_clock::now();
+auto t = duration_cast<microseconds>(stop - start);
+auto microseconds = t.count();
+
+//=============================================================
+
+template <typename Time = microseconds,
+          typename Clock = high_resolution_clock>
+struct perf_timer 
+{
     template <typename F, typename... Args>
     static Time duration(F&& f, Args... args) {
         auto start = Clock::now();
         invoke(forward<F>(f), forward<Args>(args)...);
         auto end = Clock::now();
-        return std::chrono::duration_cast<Time>(end - start);
+        return duration_cast<Time>(end - start);
     }
-}
-
-//auto start = std::chrono::high_resolution_clock::now();
-//auto end = std::chrono::high_resolution_clock::now();
-//auto diff = end - start;
-//std::chrono::duration<double> time = end - start;
-//cout << "elapsed time: " << time.count()
-//     << "\n\n";
+};
