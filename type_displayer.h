6491946e011fb1f3b1e002551788692c9520cf74
jwbat <jwesleyb@gmail.com>
2022-01-05 15:48:31 -0800

    .

--- type_displayer.h
+++ type_displayer.h
@@ -6,5 +6,5 @@ const int ans = 5;
 auto x = ans;
 auto y = &x;
 
-TD<decltype(x)> xType;  // elicit error msgs with type info
+TD<decltype(x)> xType;  //   elicit error msgs with type info
 TD<decltype(y)> yType;  //   containing types for x and y
