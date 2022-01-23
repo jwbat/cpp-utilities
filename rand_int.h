b962cb93c06c28f23a07449730fbe0cda1f9f64d
jwbat <jwesleyb@gmail.com>
2022-01-23 09:54:41 -0800

    .

--- rand_int.h
+++ rand_int.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <random> 
 
-auto random_int() {
+auto random_int() 
+{
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_int_distribution<int> dist(0, 9);
